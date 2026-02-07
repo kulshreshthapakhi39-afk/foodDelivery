cd91b58748da7bfe9bc35e8935d694a94d08b1df
https://github.com/kulshreshthapakhi39-afk <kulshreshthapakhi39@gmail.com>
2026-02-07 20:48:55 +0530

    change the code

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Restaurant Website Loaded Successfully")
+const scrollBtn = document.getElementById("scrollTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none"
+    }
+});
+
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+function subscribe() {
+    const email = document.getElementById("email").value;
+
+    if (email === "") {
+        alert("please enter your email");
+    } else {
+        alert("Thank you for subscribe");
+        document.getElementById("email").value = "";
+    }
+}
+
+window.onload = () => {
+    document.body.style.opacity = "1";
+}
